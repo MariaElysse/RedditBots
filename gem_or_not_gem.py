d71ae106f96e9a766102b9b86b309e2b821ac583
Jeremy Neptune <jerenept@gmail.com>
2012-12-30 16:50:40 -0400

    Initial Commit

---
+++ gem_or_not_gem.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""NOTE: This bot has been banned from /r/circlejerk. It's very annoying indeed."""
+
+"""Python 2.7 (as evidenced by the Unicode madness)"""
+
+import praw,time,random,pickle
+
+#Make things simpler by declaring these up here
+user_agent="""bot by /u/jerenept. Not particularly intelligent. Randomly labels comments as gems, or not."""
+user='gem_or_not_gem'
+passwd='********'
+
+#initialise the session
+
+jerenept_data=open("gem.data","r+")
+
+# This program writes a serialised list to the disk after every comment
+#(bad for SSDs I would assume)
+
+print "Loaded datafile"
+
+random.seed()
+
+session=praw.Reddit(user_agent=user_agent)
+session.login(user,passwd)
+
+#You could do that with no args for an interactive prompt
+
+already_done=pickle.load(jerenept_data)
+
+print """Success, logged in as
+User: "%s"
+Pass: "%s"
+User-agent: "%s""""" %(user,passwd,user_agent)
+circlejerk=session.get_subreddit('circlejerk')
+print "Loaded /r/circlejerk/"
+
+gong=get_redditor(user)
+
+while True:
+	for comment in circlejerk.get_comments(limit=1000):
+		if comment.body not in ["NOT GEM","GEM"] and comment.id not in already_done:
+			if random.randint(1,10)>4:
+				try:
+					comment.reply("GEM")
+					print "Responded to %s: 'GEM'" % comment.body.encode('ascii','ignore')
+				except praw.errors.RateLimitExceeded as detail:
+					print "Waiting for Reddit to stop complaining: %s minutes" %str(detail).split(' ')[9]
+					print str(detail)
+					time.sleep(int(str(detail).split(' ')[9])*60)
+					comment.reply("GEM")
+					print "Responded to %s: 'GEM'" % comment.body.encode('ascii','ignore')
+			else:
+				try:
+					comment.reply("NOT GEM")
+					print "Responded to %s: 'NOT GEM'" % comment.body.encode('ascii','ignore')
+				except praw.errors.RateLimitExceeded as detail:
+					print "Waiting for Reddit to stop complaining: %s minutes" %str(detail).split(' ')[9]
+					print str(detail)
+					time.sleep(int(str(detail).split(' ')[9])*60)
+					comment.reply("NOT GEM")
+					print "Responded to %s: 'NOT GEM'" % comment.body.encode('ascii','ignore')
+			already_done.append(comment.id)
+			pickle.dump(already_done,jerenept_data)
+			jerenept_data.close()
+			jerenept_data=open("gem.data","r+")
+			print "Waiting 120 seconds before posting again."
+			time.sleep(120)
---
+++ jerenept.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Reddit bot that posts random /r/circlejerk comments 
+to submissions on /r/botcirclejerk. 
+Python 2.7 (as evidenced by the Unicode madness)
+
+I also used this bot to practise my SQL(ite) skills. 
+Needless to say, not very much in that department"""
+
+
+import praw,time,random
+import sqlite3 as lite
+
+#Make things simpler by declaring these up here:
+
+user_agent="/r/botcirclejerk bot by /u/jerenept. Not particularly intelligent."
+user='jerenept'
+passwd='********'
+
+#Assuming there is an SQLite DB with this name in cwd.
+#it must have a table "already_done" with column "id"
+connection=lite.connect('jerenept.db')
+cursor=connection.cursor()
+
+#initialise the session
+random.seed()
+session=praw.Reddit(user_agent=user_agent)
+session.login(user,passwd)
+
+def any_comment_replies_by(replies,username):
+	result=False
+	for reply in replies:
+		if str(reply.author)==username:
+			result=True
+
+print """ Success, logged in as
+User: "%s"
+Pass: "%s"
+User-agent: "%s""""" %(user,passwd,user_agent)
+circlejerk=session.get_subreddit('circlejerk')
+print "Loaded /r/circlejerk"
+botcj=session.get_subreddit('botcirclejerk')
+print "Loaded /r/botcirclejerk"
+comment_list=[]
+
+while True:
+	for comment in circlejerk.get_comments(limit=100):
+		comment_list.append(unicode(comment.body))
+	time.sleep(2)
+	print "Obtained CJ comments"
+	botcj_hot=botcj.get_hot(limit=10)
+	print "Obtained BotCJ posts"
+	for submission in botcj_hot:
+		submission_comments=submission.all_comments_flat
+		print "Got comments from botCJ post %s" % str(submission.id)
+		print "Checking for comments by should_be_posted_in"
+		for comment in submission_comments:
+			cursor.execute("SELECT id from already_done where id='%s'" % comment.id)
+			if str(comment.author)=="should_be_posted_in" and str(comment.id)!=str(cursor.fetchone()):
+				try:
+					comment.reply(unicode(comment_list[random.randint(0,99)]))
+					cursor.execute("INSERT into already_done values('%s')" % str(comment.id))
+					connection.commit()
+				except praw.errors.RateLimitExceeded as detail:
+					print "Waiting for Reddit to stop complaining: %s minutes" %str(detail).split(' ')[9]
+					print str(detail)
+					time.sleep(int(str(detail).split(' ')[9])*60)
+					comment.reply(str(comment_list[random.randint(0,99)]))
+				print "Posted reply"
+			else:
+				print "Nothing posted."
