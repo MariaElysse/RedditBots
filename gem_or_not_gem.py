5604d1e23d697e553b7ae0bfb5f0f72a3bb58718
Jeremy Neptune <jerenept@gmail.com>
2013-06-05 02:47:43 -0400

    Saved 100% on my Unicode headache by switching to Py3k!
    <<geico_gecko>>
    
    Note the hacky rate-limit stuff might not work anymore, seeing as they changed how exceptions work. Or something.

--- gem_or_not_gem.py
+++ gem_or_not_gem.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""NOTE: This bot has been banned from /r/circlejerk. It's very annoying indeed."""
+"""NOTE: This bot has been banned from /r/circlejerk. It's very annoying indeed. 
+Interestingly enough, people thpought it was smart. Weird."""
 
-"""Python 2.7 (as evidenced by the Unicode madness)"""
+"""Python 2.6+, should work equally well with 3+ (Using imports from __future__)"""
 
-import praw,time,random,pickle
+import praw
+import time
+import random
+import pickle
 
+from __future__ import unicode_literals, print_function
 #Make things simpler by declaring these up here
 user_agent="""bot by /u/jerenept. Not particularly intelligent. Randomly labels comments as gems, or not."""
 user='gem_or_not_gem'
 passwd='********'
-
-#initialise the session
 
 gem_data=open("gem.data","r+")
 
@@ -21,6 +24,7 @@ gem_data=open("gem.data","r+")
 print "Loaded datafile"
 
 random.seed()
+#initialise the session
 
 session=praw.Reddit(user_agent=user_agent)
 session.login(user,passwd)
@@ -29,12 +33,12 @@ session.login(user,passwd)
 
 already_done=pickle.load(gem_data)
 
-print """Success, logged in as
-User: "%s"
-Pass: "%s"
-User-agent: "%s""""" %(user,passwd,user_agent)
+print("""Success, logged in as
+User: "{0}"
+Pass: "{1}"
+User-agent: "{2}""""".format(user,passwd,user_agent)
 circlejerk=session.get_subreddit('circlejerk')
-print "Loaded /r/circlejerk/"
+print("Loaded /r/circlejerk/")
 
 while True:
   for comment in circlejerk.get_comments(limit=1000):
@@ -42,26 +46,26 @@ while True:
 			if random.randint(1,10)>4:
 				try:
 					comment.reply("GEM")
-					print "Responded to %s: 'GEM'" % comment.body.encode('ascii','ignore')
+					print("Responded to {0}: 'GEM'".format(comment.body)
 				except praw.errors.RateLimitExceeded as detail:
-					print "Waiting for Reddit to stop complaining: %s minutes" %str(detail).split(' ')[9]
-					print str(detail)
+					print( " Rate Limited. Waiting for Reddit to stop complaining: {0} minutes" .format(str(detail).split(' ')[9]) )
+					print( str(detail))
 					time.sleep(int(str(detail).split(' ')[9])*60)
 					comment.reply("GEM")
-					print "Responded to %s: 'GEM'" % comment.body.encode('ascii','ignore')
+					print("Responded to {0}: 'GEM'".format(comment.body))
 			else:
 				try:
 					comment.reply("NOT GEM")
-					print "Responded to %s: 'NOT GEM'" % comment.body.encode('ascii','ignore')
+					print("Responded to {0}: 'NOT GEM'".format(comment.body))
 				except praw.errors.RateLimitExceeded as detail:
-					print "Waiting for Reddit to stop complaining: %s minutes" %str(detail).split(' ')[9]
-					print str(detail)
+					print( "Rate Limited. Waiting for Reddit to stop complaining: {0} minutes".format(str(detail).split(' ')[9]) )
+					print(str(detail))
 					time.sleep(int(str(detail).split(' ')[9])*60)
 					comment.reply("NOT GEM")
-					print "Responded to %s: 'NOT GEM'" % comment.body.encode('ascii','ignore')
+					print("Responded to {0}: 'NOT GEM'".format(comment.body))
 			already_done.append(comment.id)
 			pickle.dump(already_done,gem_data)
 			gem_data.close()
 			gem_data=open("gem.data","r+")
-			print "Waiting 120 seconds before posting again."
+			print( "Waiting 120 seconds before posting again." )
 			time.sleep(120)
